f8bfe86a144e20037a3daa08037bca1b2f49030b
AlaaBenk94 <a.benkarrad@gmail.com>
2019-03-27 17:02:00 +0100

    downloaded source code

---
+++ Code source/Haar Cascades/main.py
@@ -0,0 +1,28 @@
+from imutils import face_utils
+import dlib
+import cv2
+import numpy as np 
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, img = cap.read()
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	'''cv2.imshow('frame', frame)'''
+
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+	    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+	cv2.namedWindow('color',cv2.WINDOW_NORMAL)
+	cv2.resizeWindow('color', 600,400 )
+	cv2.imshow('color', img)
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cap.release()
+cv2.destroyAllWindows()
