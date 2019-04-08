019473c3446d60b0c3a44319b389a1eb42d047dd
walidone <walidonehafiane@gmail.com>
2019-04-08 19:30:53 +0200

    new code

---
+++ new_code/face_change_detection/__init__.py
@@ -0,0 +1,4 @@
+import random
+from face_change_detection.clasifieur import *
+from face_change_detection.tests.affichage import affichebase
+
---
+++ new_code/face_change_detection/video_main.py
@@ -0,0 +1,47 @@
+import cv2
+import time
+
+import numpy as np
+
+from detection_point import *
+from caracterestique import *
+import affichage_car
+
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+imgsize=img.shape
+fdet= facedetection()
+car=caracterestique(imgsize)
+seuil=10
+aff= affichage_car.affichage()
+while(True):
+    ret, img = cap.read()
+    #imgdet=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #imgdeteg=cv2.equalizeHist(img)
+    vect,imgdeteg=fdet.get_vecteur(img)
+
+    if(vect!={}):
+        zdis=car.distence(vect)
+        print(" la distence =",zdis)
+        difx, dify, dis=car.mov(vect)
+        if (difx > seuil):
+            print("move gauche")
+        if (difx < -seuil):
+            print("move droite")
+        if (dify < -seuil):
+            print("move bas")
+        if (dify > seuil):
+            print("move haut")
+        o=car.overture_bouche(vect)
+        print("ouverture de bouche",o)
+        imgdeteg = aff.aff(imgdeteg,[o,zdis,dis])
+    #cv2.imshow('',imgdet)
+
+
+    cv2.imshow('eg',imgdeteg)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+     break
+
+cap.release()
+cv2.destroyAllWindows()
